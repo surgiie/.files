bf204281f4dca0d44c7ac5b763b63e163e2cb91b
Sergio Compean <scompean24@gmail.com>
2025-02-23 14:26:20 -0500

    Save dotfiles.

--- symlinks/.config/nvim/lua/core/options.lua
+++ symlinks/.config/nvim/lua/core/options.lua
@@ -44,8 +44,6 @@ vim.opt.iskeyword:append("-") -- Hyphenated words recognized by searches (defaul
 vim.opt.formatoptions:remove({ "c", "r", "o" }) -- Don't insert the current comment leader automatically for auto-wrapping comments using 'textwidth', hitting <Enter> in insert mode, or hitting 'o' or 'O' in normal mode. (default: 'croql')
 vim.opt.runtimepath:remove("/usr/share/vim/vimfiles") -- Separate Vim plugins from Neovim in case Vim still in use (default: includes this path if Vim is installed)
 vim.opt.virtualedit = "all" -- Allow cursor to move where there is no actual character (default: '')
-vim.g.terminal_sixel = false -- Disable Sixel graphics in terminal
-
 vim.opt.fillchars = { -- Set fillchars to better values
 	horiz = "━",
 	horizup = "┻",
