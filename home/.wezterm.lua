a47a799ed14495eec5b340d091d52417519f83ed
Sergio Compean <scompean24@gmail.com>
2026-08-13 13:03:42 -0400

    chore: preferences and flake updates

--- home/.wezterm.lua
+++ home/.wezterm.lua
@@ -4,6 +4,7 @@ local common = dofile(wezterm.home_dir .. "/.wezterm.common.lua")
 
 config.enable_wayland = true
 config.font_size = 16
+config.front_end = "Software"
 
 local user = os.getenv("USER")
 config.background = {
